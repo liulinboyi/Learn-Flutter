6206edc9f6f0acb276a97e86c14c9f3fea99529d
liulinboyi <814921718@qq.com>
2019-06-18 15:56:50 +0800

    first

--- lib/main.dart
+++ lib/main.dart
@@ -112,26 +112,159 @@
 //  }
 //}
 
-//学习的例子
+// //学习的例子
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+//导入相对路径插件
+import 'package:path_provider/path_provider.dart';
+//设置Placeholder
+import 'package:transparent_image/transparent_image.dart';
 
 void main() => runApp(new MyApp());
+//获取 SDCard路径
+
+// class MyApp extends StatelessWidget {
+
+//   Future<File> _getLocalFile(String filename) async {
+//     String dir = (await getExternalStorageDirectory()).path;
+//     File f = new File('$dir/$filename');
+//     debugPrint(f.toString());
+//     return f;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//         title: 'Flutter bottomNavigationBar',
+//         theme: new ThemeData.fallback(),
+// //加载网络图片
+// //      home: Image.network(
+// //        'https://upload.jianshu.io/users/upload_avatars/7935065/7a1306bb-6ca4-4bc9-9c7f-10eed04e81b5?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120',
+// //      ),
+// //加载 assets图片
+//         // home: Image(
+//         //   height: 100,
+//         //   width: 100,
+//         //   image: AssetImage("images/avator.png"),
+//         // ));
+//         home: FutureBuilder(
+//             future: _getLocalFile("Pictures/demo.jpg"),
+//             builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+//               debugPrint(snapshot.toString());
+//               debugPrint(snapshot.data.toString());
+
+//               return snapshot.data != null
+//                   ? Image.file(snapshot.data)
+//                   : Container();
+//             }));
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        title: 'Flutter bottomNavigationBar',
-        theme: new ThemeData.fallback(),
-//加载网络图片
-//      home: Image.network(
-//        'https://upload.jianshu.io/users/upload_avatars/7935065/7a1306bb-6ca4-4bc9-9c7f-10eed04e81b5?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120',
-//      ),
-//加载 assets图片
-        home: Image(
-          height: 100,
-          width: 100,
-          image: AssetImage("images/avator.png"),
-        ));
+    final title = 'Fade in images';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Stack(
+          children: <Widget>[
+            Center(child: CircularProgressIndicator()),
+            Center(
+                //本地图片占位图
+                child: FadeInImage.assetNetwork(
+              placeholder: 'images/loading.gif',
+              image: 'http://www.devio.org/img/avatar.png',
+            )
+                //内存图片加载占位图
+                // child: FadeInImage.memoryNetwork(
+                //   placeholder: kTransparentImage,
+                //   image: 'http://www.devio.org/img/avatar.png',
+                // ),
+                ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+//计数器 本地存储 实例
+
+// import 'dart:io';
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:path_provider/path_provider.dart';
+
+// void main() => runApp(new FileOperationRoute());
+
+// class FileOperationRoute extends StatefulWidget {
+//   FileOperationRoute({Key key}) : super(key: key);
+
+//   @override
+//   _FileOperationRouteState createState() => new _FileOperationRouteState();
+// }
+
+// // //获取 SDCard路径
+
+// // class MyApp extends StatelessWidget {
+
+// class _FileOperationRouteState extends State<FileOperationRoute> {
+//   int _counter;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     //从文件读取点击次数
+//     _readCounter().then((int value) {
+//       setState(() {
+//         _counter = value;
+//       });
+//     });
+//   }
+
+//   Future<File> _getLocalFile() async {
+//     // 获取应用目录
+//     String dir = (await getApplicationDocumentsDirectory()).path;
+//     return new File('$dir/counter.txt');
+//   }
+
+//   Future<int> _readCounter() async {
+//     try {
+//       File file = await _getLocalFile();
+//       // 读取点击次数（以字符串）
+//       String contents = await file.readAsString();
+//       return int.parse(contents);
+//     } on FileSystemException {
+//       return 0;
+//     }
+//   }
+
+//   Future<Null> _incrementCounter() async {
+//     setState(() {
+//       _counter++;
+//     });
+//     // 将点击次数以字符串类型写到文件中
+//     await (await _getLocalFile()).writeAsString('$_counter');
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(title: new Text('文件操作')),
+//       body: new Center(
+//         child: new Text('点击了 $_counter 次'),
+//       ),
+//       floatingActionButton: new FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: new Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
