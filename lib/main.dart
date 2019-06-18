a45922d8bee5eaa2e3ae1773aecb9bb685032c5a
liulinboyi <814921718@qq.com>
2019-06-18 16:21:45 +0800

    图片缓存

--- lib/main.dart
+++ lib/main.dart
@@ -120,6 +120,8 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 //设置Placeholder
 import 'package:transparent_image/transparent_image.dart';
+//设置图片缓存
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(new MyApp());
 //获取 SDCard路径
@@ -161,10 +163,45 @@ void main() => runApp(new MyApp());
 //   }
 // }
 
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final title = 'Fade in images';
+
+//     return MaterialApp(
+//       title: title,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text(title),
+//         ),
+//         body: Stack(
+//           children: <Widget>[
+//             Center(child: CircularProgressIndicator()),
+//             Center(
+//                 //本地图片占位图
+//                 child: FadeInImage.assetNetwork(
+//               placeholder: 'images/loading.gif',
+//               image: 'http://www.devio.org/img/avatar.png',
+//             )
+//                 //内存图片加载占位图
+//                 // child: FadeInImage.memoryNetwork(
+//                 //   placeholder: kTransparentImage,
+//                 //   image: 'http://www.devio.org/img/avatar.png',
+//                 // ),
+//                 ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+//设置图片缓存
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Fade in images';
+    final title = 'Cached Images';
 
     return MaterialApp(
       title: title,
@@ -172,99 +209,13 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: Stack(
-          children: <Widget>[
-            Center(child: CircularProgressIndicator()),
-            Center(
-                //本地图片占位图
-                child: FadeInImage.assetNetwork(
-              placeholder: 'images/loading.gif',
-              image: 'http://www.devio.org/img/avatar.png',
-            )
-                //内存图片加载占位图
-                // child: FadeInImage.memoryNetwork(
-                //   placeholder: kTransparentImage,
-                //   image: 'http://www.devio.org/img/avatar.png',
-                // ),
-                ),
-          ],
+        body: Center(
+          child: CachedNetworkImage(
+            placeholder: (context, url) => new CircularProgressIndicator(),
+            imageUrl: 'https://picsum.photos/250?image=9',
+          ),
         ),
       ),
     );
   }
 }
-
-//计数器 本地存储 实例
-
-// import 'dart:io';
-// import 'dart:async';
-// import 'package:flutter/material.dart';
-// import 'package:path_provider/path_provider.dart';
-
-// void main() => runApp(new FileOperationRoute());
-
-// class FileOperationRoute extends StatefulWidget {
-//   FileOperationRoute({Key key}) : super(key: key);
-
-//   @override
-//   _FileOperationRouteState createState() => new _FileOperationRouteState();
-// }
-
-// // //获取 SDCard路径
-
-// // class MyApp extends StatelessWidget {
-
-// class _FileOperationRouteState extends State<FileOperationRoute> {
-//   int _counter;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     //从文件读取点击次数
-//     _readCounter().then((int value) {
-//       setState(() {
-//         _counter = value;
-//       });
-//     });
-//   }
-
-//   Future<File> _getLocalFile() async {
-//     // 获取应用目录
-//     String dir = (await getApplicationDocumentsDirectory()).path;
-//     return new File('$dir/counter.txt');
-//   }
-
-//   Future<int> _readCounter() async {
-//     try {
-//       File file = await _getLocalFile();
-//       // 读取点击次数（以字符串）
-//       String contents = await file.readAsString();
-//       return int.parse(contents);
-//     } on FileSystemException {
-//       return 0;
-//     }
-//   }
-
-//   Future<Null> _incrementCounter() async {
-//     setState(() {
-//       _counter++;
-//     });
-//     // 将点击次数以字符串类型写到文件中
-//     await (await _getLocalFile()).writeAsString('$_counter');
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Scaffold(
-//       appBar: new AppBar(title: new Text('文件操作')),
-//       body: new Center(
-//         child: new Text('点击了 $_counter 次'),
-//       ),
-//       floatingActionButton: new FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: new Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
